5d82c6362ea85eafa3c7d3364d6ad52a93872bbf
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2020-03-27 14:18:47 +0200

    Add Login and Register components

---
+++ src/components/core/AppLogin.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field prepend-icon="mdi-account-circle" label="Username" />
+        <v-text-field
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          @click:append="showPassword = !showPassword"
+        />
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'AppLogin',
+  data: () => ({
+    showPassword: false
+  })
+};
+</script>
---
+++ src/components/core/AppRegister.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Register</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field prepend-icon="mdi-account-circle" label="Username" />
+        <v-text-field prepend-icon="mdi-lock" label="Password" type="password" />
+        <v-text-field prepend-icon="mdi-lock" label="Retype Password" type="password" />
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Login</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Register</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'AppLogin',
+  data: () => ({})
+};
+</script>

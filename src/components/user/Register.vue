58f4e791d555313358fbe9e9c7caabd1a887cf9b
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2020-04-07 00:19:13 +0300

    Add validation on register form

--- src/components/user/Register.vue
+++ src/components/user/Register.vue
@@ -1,67 +1,107 @@
 <template>
-  <v-card class="mx-auto mt-5" max-width="400">
-    <v-card-title class="title font-weight-regular justify-space-between">
-      <span>{{ currentTitle }}</span>
-      <v-avatar color="primary lighten-2" class="subheading white--text" size="24" v-text="step"></v-avatar>
-    </v-card-title>
+  <v-form ref="form" v-model="valid" @submit.prevent>
+    <v-card class="mx-auto mt-5" max-width="400">
+      <v-card-title class="title font-weight-regular justify-space-between">
+        <span>{{ currentTitle }}</span>
+        <v-avatar color="primary lighten-2" class="subheading white--text" size="24" v-text="step"></v-avatar>
+      </v-card-title>
 
-    <v-window v-model="step">
-      <v-window-item :value="1">
-        <v-card-text>
-          <v-text-field prepend-icon="mdi-account-circle" label="Email"></v-text-field>
-          <span
-            class="caption grey--text text--darken-1"
-          >This is the email you will use to login to your EventOur account</span>
-        </v-card-text>
-      </v-window-item>
+      <v-window v-model="step">
+        <v-window-item :value="1">
+          <v-card-text>
+            <v-text-field
+              prepend-icon="mdi-account-circle"
+              label="Email"
+              name="email"
+              type="email"
+              value=""
+              v-model="email"
+              :rules="emailRules"
+              required
+            ></v-text-field>
+            <span class="caption grey--text text--darken-1">This is the email you will use to login to your EventOur account</span>
+          </v-card-text>
+        </v-window-item>
 
-      <v-window-item :value="2">
-        <v-card-text>
-          <v-text-field prepend-icon="mdi-lock" label="Password" type="password"></v-text-field>
-          <v-text-field prepend-icon="mdi-lock" label="Confirm Password" type="password"></v-text-field>
-          <span class="caption grey--text text--darken-1">Please enter a password for your account</span>
-        </v-card-text>
-      </v-window-item>
-      <v-window-item :value="3">
-        <div class="pa-4 text-center">
-          <v-img
-            class="mb-4"
-            contain
-            height="128"
-            src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-          ></v-img>
-          <h3 class="title font-weight-light mb-2">Welcome to EventOur</h3>
-          <span class="caption grey--text">Thanks for signing up!</span>
-        </div>
-      </v-window-item>
-    </v-window>
+        <v-window-item :value="2">
+          <v-card-text>
+            <v-text-field
+              prepend-icon="mdi-lock"
+              label="Password"
+              name="password"
+              type="password"
+              v-model="password"
+              :rules="passwordRules"
+              required
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-lock"
+              label="Confirm Password"
+              name="confirm-password"
+              v-model="confirmPassword"
+              type="password"
+              :rules="comparePasswords"
+            ></v-text-field>
+            <span class="caption grey--text text--darken-1">Please enter a password for your account</span>
+          </v-card-text>
+        </v-window-item>
+        <v-window-item :value="3">
+          <div class="pa-4 text-center">
+            <v-img class="mb-4" contain height="128" src="https://cdn.vuetifyjs.com/images/logos/v.svg"></v-img>
+            <h3 class="title font-weight-light mb-2">Welcome to EventOur</h3>
+            <span class="caption grey--text">Thanks for signing up!</span>
+          </div>
+        </v-window-item>
+      </v-window>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-btn :disabled="step === 1" text @click="step--">Back</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn :disabled="step === 3" color="info" depressed @click="step++">Next</v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-actions>
+        <v-btn v-if="step === 1" color="success" to="/user/login">Login</v-btn>
+        <v-btn v-if="step === 2" @click="step--" color="info" :disabled="!valid">Back</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn v-if="step === 1" color="primary" @click="step++" :disabled="!valid" type="button">Next</v-btn>
+        <v-btn v-else-if="step === 2" color="primary" type="submit" @click="submitRegistrationHandler" :disabled="!valid">Sign up</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 <script>
 export default {
   data: () => ({
-    step: 1
+    valid: true,
+    step: 1,
+    email: '',
+    emailRules: [(v) => !!v || 'E-mail is required', (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
+    password: '',
+    passwordRules: [(v) => !!v || 'Password is required', (v) => !(v && v.length < 3) || 'Password must be at least 3 characters'],
+    confirmPassword: '',
   }),
 
   computed: {
     currentTitle() {
       switch (this.step) {
         case 1:
-          return "Register";
+          return 'Register';
         case 2:
-          return "Create a password";
+          return 'Create a password';
         default:
-          return "Account created";
+          return 'Account created';
       }
-    }
-  }
+    },
+    comparePasswords() {
+      // console.log(this.email, this.password, this.confirmPassword);
+      return this.password !== this.confirmPassword ? ['Passwords do not match'] : [];
+    },
+  },
+  methods: {
+    submitRegistrationHandler() {
+      // console.log(this.email, this.password, this.confirmPassword);
+      this.step++;
+    },
+    validateForm() {
+      this.$refs.form.validate();
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import vuetify from './plugins/vuetify';
 import routes from './router';
 import store from './store';
 import dateFilter from './filters/date';
+import * as firebase from 'firebase';
 
 
 const router = new VueRouter(routes);
@@ -18,5 +19,14 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDTpEYNM6XNokr3bZOghSCmQrVyX8ic-nw',
+      authDomain: 'eventour-63336.firebaseapp.com',
+      databaseURL: 'https://eventour-63336.firebaseio.com',
+      projectId: 'eventour-63336',
+      storageBucket: '',  
+    })
+  }
 }).$mount('#app')
